0ae10a22ee34481b845d3eda6965f1a395cc06d7
Claudia Lujan <verenisse.eyzaguirre@gmail.com>
2022-03-05 21:19:56 -0500

    add random value to hashid,configure w friendlyid

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -1,2 +1,13 @@
 class Incident < ApplicationRecord
+  extend FriendlyId
+  friendly_id :hashid, use: :slugged
+
+  before_validation :set_hashid, prepend:true, if: Proc.new { |incident| incident.hashid.nil? }
+
+  private
+
+  def set_hashid
+    self.hashid = SecureRandom.urlsafe_base64(8)
+  end
+
 end
